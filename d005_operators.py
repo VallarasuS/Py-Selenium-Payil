65f838c73fe36f2a8adb6133f1baddc445ffefc2
Vallarasu Sambathkumar <vals@live.in>
2025-12-13 20:16:36 +0530

    Comparison Operator

---
+++ d005_operators.py
@@ -0,0 +1,37 @@
+x = 100
+y = 150
+z = x + y
+
+print(z)
+print(2**3)
+print(10 % 3)
+
+revenue = 20000
+salary = 8000
+head_count = 2
+
+net_payable = head_count * salary
+print(net_payable)
+
+profit = revenue - net_payable
+print(profit)
+
+profit = revenue % salary
+print(profit)
+
+age = 20
+min_age = 18
+can_vote = age > min_age
+
+print(can_vote)
+
+
+# Comparison Operator Practice
+# 1. Given age check if user can vote. min age 18
+# 2. Given age check if user can drive min age 18
+# 3. Given mark check if user fail. pass mark 36
+# 4. Given mark check if user scored centum (100)
+# 5. Given body temperature check if user has fever (more than 100)
+# 6. Given number check if it is odd
+# 7. Given number check if it is even
+# 8. Given number check if it prime number
