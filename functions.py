fa81697443904cc02e0dd8473996aa340779844d
Vallarasu Sambathkumar <vals@live.in>
2025-12-20 07:24:02 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,76 +1,35 @@
+# def add(x, y):
+#     z = x + y
+#     print(z)
+
+
 def hello():
     print("Hello")
-    print("Welcome to Python")
-
-
-hello()
 
 
 def add(x, y):
-    print("X:", x)
-    print("Y:", y)
-    print("Sum X + Y :", x + y)
+    print("Hello from Add")
+    print(x + y)
 
 
-add(0, 1)
-add(2, 1)
-add(2, 3)
+# hello()
+# add(10, 20)
 
 
-def mul(a, b):
-    print("A :", a)
-    print("B :", b)
-    print("Mul A * B =", a * b)
+def div(x, y):
+    z = x / y
+    print(z)
 
 
-mul(2, 3)
+div(10, 20)
+div(10, 2)
+div(9, 3)
+div(12, 4)
+
+print("---EOF---")
 
 
-def login(user_name, password):
-    print("Open Browser")
-    print("Open URL")
-    print("User Name:", user_name)
-    print("Password: ", password)
-    print("Login")
+# add, div, mul, sub, floor_div,
+# can_vote, is_minor, is_centum, has_fever
 
-    username_valid = user_name == "John"
-    password_valid = password == "1234"
-    return username_valid and password_valid
-
-
-login_result = login("John", "1234")
-print("Login Result:", login_result)
-
-print("-------")
-
-login_result = login("John_", "1234")
-print("Login Result:", login_result)
-
-print("-------")
-
-login_result = login("John", "1234_")
-print("Login Result:", login_result)
-
-login("John", "1234_")
-login("John_", "1234_")
-
-print("EOF")
-
-
-def add(x, y):
-    return x + y
-
-
-result = add(2, 3)
-print(result)
-
-# Try yourself
-# add,
-# mul,
-# sub,
-# pow,
-# div,
-# floor_div
-
-# print
-# return
+# functions_.py
