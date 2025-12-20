e6b006d4c8ce4bb9aa93e43677fee09bcc15a11c
Vallarasu Sambathkumar <vals@live.in>
2025-12-20 08:05:42 +0530

    print, input & data types

---
+++ datatypes.py
@@ -0,0 +1,32 @@
+print("Hello World")
+# print("Hello World")
+# print("""Hello World""")
+
+# Ctr; + / -> Comment
+
+# name = input("Enter your name: ")
+# print("Hello ", name)
+
+age = 30
+print(age)
+print(type(age))
+
+weight = 56.8
+print(weight)
+print(type(weight))
+
+name = "John"
+print(name)
+print(type(name))
+
+can_vote = True
+print(can_vote)
+print(type(can_vote))
+
+age = input("Enter your age: 30")
+age = int(age)
+print(age)
+print(type(age))
+
+# name, age, height, weight
+# qualification, employer, license holder?
