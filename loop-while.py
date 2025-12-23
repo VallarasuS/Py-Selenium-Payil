387afa909b5a297e0eef94398532885b14395310
Vallarasu Sambathkumar <vals@live.in>
2025-12-23 19:18:29 +0530

    While

---
+++ loop-while.py
@@ -0,0 +1,47 @@
+# x = 0
+# x = x + 1
+# print(x)
+
+# x = x + 1
+# print(x)
+
+# x = x + 1
+# print(x)
+
+# x = x + 1
+# print(x)
+
+# x = x + 1
+# print(x)
+
+
+# def add(x):
+#     x = x + 1
+#     print(x)
+#     return x
+
+
+# x = 0
+# x = add(x)
+# x = add(x)
+# x = add(x)
+# x = add(x)
+# x = add(x)
+
+x = 0
+
+while x < 100:
+    x = x + 1
+    print(x)
+
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
