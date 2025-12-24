d5c1bfb6742b451289831812a9d7488384d1e486
Vallarasu Sambathkumar <vals@live.in>
2025-12-24 13:02:30 +0530

    for loop

---
+++ for-example.py
@@ -0,0 +1,25 @@
+# 1, 2, 3, 4, 5 -> 6
+# 5, 10, 15, 20 -> 25
+
+
+start = 5
+end = 25
+step = 5
+
+# i = start
+# while i < end:
+#     i = i + step
+#     print(i)
+
+# range -> start, end, step
+
+start = 1
+end = 11
+step = 1
+for i in range(start, end, step):
+    print(i)
+
+# for i in range(1, 11, 1):
+#     print(i, " * 3 = ", i * 3)
+
+# table 3, 5, 6
--- while-examples.py
+++ while-examples.py
@@ -1,91 +1,140 @@
-# x = 0
-# x = x + 1
-# print(x)
+# # x = 0
+# # x = x + 1
+# # print(x)
 
-# x = x + 1
-# print(x)
+# # x = x + 1
+# # print(x)
 
-# x = x + 1
-# print(x)
+# # x = x + 1
+# # print(x)
 
-# x = x + 1
-# print(x)
+# # x = x + 1
+# # print(x)
 
-# x = x + 1
-# print(x)
+# # x = x + 1
+# # print(x)
 
-# # def add_print(x):
-# #     x = x + 1
-# #     print(x)
-# #     return x
+# # # def add_print(x):
+# # #     x = x + 1
+# # #     print(x)
+# # #     return x
 
+
+# # # x = 0
+# # # x = add_print(x)
+# # # x = add_print(x)
+# # # x = add_print(x)
+# # # x = add_print(x)
+# # # x = add_print(x)
 
 # # x = 0
-# # x = add_print(x)
-# # x = add_print(x)
-# # x = add_print(x)
-# # x = add_print(x)
-# # x = add_print(x)
+# # while x < 5:
+# #     x = x + 1
+# #     print(x)
 
-# x = 0
-# while x < 5:
-#     x = x + 1
+# # print("---")
+
+# # x = 0
+# # while x < 10:
+# #     x = x + 2
+# #     print(x)
+
+
+# total = 0
+
+# total_one = 1 + 2 + 3 + 4 + 5
+# print(total_one)  # -> 15
+
+# total = total + 1
+# total = total + 2
+# total = total + 3
+# total = total + 4
+# total = total + 5
+
+# print(total)
+
+# i = 1
+# sum = 0
+# while i <= 5:
+#     sum = sum + i
+#     i = i + 1
+
+# print(sum)
+
+# i = 1
+# sum = 0
+# while i <= 5:
+#     sum = sum + 1
+#     # print(i)
+#     i = i + 1
+
+# sum = 15
+
+# # 1
+# # 2
+# # 3
+# # 4
+# # 5
+
+# 1, 6, 11, 16
+# 5, 10, 15, 20
+
+# i = 3
+# count = 2
+
+# while i <= 4:
+#     count = count + 1
+#     x = i * 5
 #     print(x)
+#     i = i + 1
 
-# print("---")
-
-# x = 0
-# while x < 10:
-#     x = x + 2
-#     print(x)
+# print(count)
 
 
+# 0, 1
+# <, <=
+# end? 20? 25?
+# i + 5, * 5
+
+# # count values between 1-20, in increments of 3
+# # count values between 1-20, in increments of 2
+# # count values between 1-20, in increments of 5
+
+# i = 3
+# count = 2
+# while i <= 4:
+#     count = count + 1
+#     print(i * 5)
+#     i = i + 1
+
+# print(count)
+
+# total -> [1, 2, 3, 4] -> 10
+
+i = 0
 total = 0
 
-total_one = 1 + 2 + 3 + 4 + 5
-print(total_one)  # -> 15
-
-total = total + 1
-total = total + 2
-total = total + 3
-total = total + 4
-total = total + 5
-
-print(total)
-
-i = 1
-sum = 0
-while i <= 5:
-    sum = sum + i
+while i < 4:
     i = i + 1
+    print(i)
+    total = total + i
+
+print("Total:", total)
+
+x = 1
+sum = 0
+
+while x <= 4:
+    print(x)
+    sum = sum + x
+    x = x + 1
 
 print(sum)
 
-i = 1
-sum = 0
-while i <= 5:
-    sum = sum + 1
-    # print(i)
-    i = i + 1
-
-sum = 15
-
-# 1
-# 2
-# 3
-# 4
-# 5
-
-count = 0
-
-i = 1
-while i <= 20:
-    count = count + 1
-    print(i)
-    i = i + 2
-
-print(count)
-
-# count values between 1-20, in increments of 3
-# count values between 1-20, in increments of 2
-# count values between 1-20, in increments of 5
+# 1, 2, 3, 4
+# sum = 0
+# sum = sum + num
+# sum = 0 + 1 -> 1
+# sum = 1 + 2 -> 3
+# sum = 3 + 3 -> 6
+# sum = 6 + 4 -> 10
