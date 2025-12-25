007455f7bd3fc4a742a2bc6d5a27f9dfe84058c2
Vallarasu Sambathkumar <vals@live.in>
2025-12-25 17:49:37 +0530

    Create for-intro.py

---
+++ for-intro.py
@@ -0,0 +1,24 @@
+start = 3
+stop = 30
+step = 3
+
+while start < stop:
+    print(start)
+    start = start + step
+
+for x in range(start, stop, step):
+    print(x)
+
+# 1, 2, 3, ... 10
+# start = 1
+# stop = 11
+# step = 1
+
+# for i in range(start, stop, step):
+#     print(i)
+
+# for b in range(2, 22, 2):
+#     print(b)
+
+# for i in range(5, 55, 5):
+#     print(i)
