eae2fb723230381a447d9ae2d670ce5643370d71
Vallarasu Sambathkumar <vals@live.in>
2026-01-05 12:56:03 +0530

    Create list-examples.py

---
+++ list-examples.py
@@ -0,0 +1,15 @@
+# create list
+
+# square bracket
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+
+# list
+even_numbers = list([2, 4, 6, 8])
+print(even_numbers)
+
+# from another sequence
+hello_list = list("Hello")
+print(hello_list)
+
+print(type(hello_list))
