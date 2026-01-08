ff922a275b0824a231ca5b79334b31770311f177
Vallarasu Sambathkumar <vals@live.in>
2026-01-08 18:51:56 +0530

    Create list-creation.py

---
+++ list-creation.py
@@ -0,0 +1,28 @@
+numbers = [5, 10, 15, 20, 25]
+total = sum(numbers)
+top = max(numbers)
+print(total)
+print(top)
+
+text = ["Hello", "world"]
+size = len(text)
+print(size)
+
+mixed = [1, "Hi", True, 3.14, text]
+
+
+for i in range(1, 11, 1):
+    print(i)
+
+print("---")
+
+numbers = [5, 10, 15, 20, 25]
+for i in numbers:
+    print(i)
+
+# [] -> Create
+# list() -> Create
+# sum - total
+# max - top value
+# len - size of list
+# loop - iterate over list
