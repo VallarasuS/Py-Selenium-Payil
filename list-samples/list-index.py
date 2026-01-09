35f3c29fddd156b3345e9e32c575406ba1e4890d
Vallarasu Sambathkumar <vals@live.in>
2026-01-09 12:47:37 +0530

    move list files

---
+++ list-samples/list-index.py
@@ -0,0 +1,40 @@
+numbers = [1, 2, 3, 4]
+
+x = numbers[0]
+print(x)
+
+numbers[0] = 10
+print(numbers)
+
+# len
+# max
+# sum
+
+total = sum(numbers)
+top = max(numbers)
+size = len(numbers)
+
+print(top, size, total)
+
+# # Add
+# numbers.append(5)
+# numbers.insert(0, 0)
+# print(numbers)
+
+# # Delete
+# x = numbers.pop()
+# x = numbers.pop(0)
+# x = numbers.remove(3)
+# print(numbers)
+
+# # Print list methods
+# print(dir(list))
+
+# for i in numbers:
+#     print(i)
+
+# total = 0
+# for i in numbers:
+#     total = total + i
+
+# print("Total:", total)
