da9f39e760567c28b21559842e07acca29e751a3
Vallarasu Sambathkumar <vals@live.in>
2026-01-12 18:46:44 +0530

    list transform every element

---
+++ list-acc.py
@@ -0,0 +1,27 @@
+# Accumulation
+
+# transform - water heater, cooler
+# filter - remove sediments, minerals
+# reduce / collection - bucket, ex: total, multiplication
+# find - find an yellow ball out of many
+
+
+numbers = [1, 2, 3, 4]
+even = []
+
+for n in numbers:
+    result = n * 2
+    even.append(result)
+
+print(numbers)
+print(even)
+
+
+text = ["Hi", "Hello", "Hey"]
+capitalized = []
+
+for word in text:
+    word = word.upper()
+    capitalized.append(word)
+
+print(capitalized)
