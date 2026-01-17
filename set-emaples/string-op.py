be3ff3461fccb8373742255691aafeb0601cfd4b
Vallarasu Sambathkumar <vals@live.in>
2026-01-17 18:22:37 +0530

    string op

---
+++ set-emaples/string-op.py
@@ -0,0 +1,18 @@
+data = "Hello This is a Python Program"
+
+up = data.upper()
+print(up)
+
+lo = data.lower()
+print(lo)
+
+cap = lo.title()
+print(cap)
+
+res = data.startswith("Hello")
+print("Start ?", res)
+
+# ends with
+# find
+# split
+# join
---
+++ tuple-examples/tuple-iteration.py
@@ -0,0 +1,15 @@
+data = ("John", 30, 54.5, "Engineer")
+print(data)
+
+for i in data:
+    print(i)
+
+numbers = (1, 2, 3, 4)
+total = sum(numbers)
+print(total)
+
+size = len(numbers)
+print(size)
+
+max(numbers)
+min(numbers)
