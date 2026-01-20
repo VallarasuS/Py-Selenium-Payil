cf4c39ca41c9a4a12d71c7d74a91e8e2fbe728d7
Vallarasu Sambathkumar <vals@live.in>
2026-01-20 19:01:27 +0530

    Update dict-creation.py

--- dict-examples/dict-creation.py
+++ dict-examples/dict-creation.py
@@ -1,30 +1,36 @@
-score = {"tamil": 56, "english": 65, "math": 64}
+# score = {"tamil": 56, "english": 65, "math": 64}
 
-print(score)
-print(type(score))
-# print(dir(score))
+# print(score)
+# print(type(score))
+# # print(dir(score))
 
-# get ("tamil") -> 56
-# update ({ "tamil": 60, "science": 45 })
-# get ("tamil") -> 60
+# # get ("tamil") -> 56
+# # update ({ "tamil": 60, "science": 45 })
+# # get ("tamil") -> 60
 
-ta = score.get("tamil")
-print(ta)
+# ta = score.get("tamil")
+# print(ta)
 
-score.update({"tamil": 60})
-score.update({"science": 60})
-print(score)
+# score.update({"tamil": 60})
+# score.update({"science": 60})
+# score["english"] = 90
+# score["computer"] = 98
+# print(score)
 
-# items = score.items()
-# print(items)
+# # items = score.items()
+# # print(items)
 
-# values = score.values()
-# print(values)
+# # values = score.values()
+# # print(values)
 
-# keys = score.keys()
-# print(keys)
+# # keys = score.keys()
+# # print(keys)
 
-# NO-SQL
-# Key-Value Pairs
-# Redis
-# Mongo-DB
+# # NO-SQL
+# # Key-Value Pairs
+# # Redis
+# # Mongo-DB
+
+fruits = {"apple": 10}
+
+count = fruits.get("apple")
