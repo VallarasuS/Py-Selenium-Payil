17945d9ad7607d90e46a65ff7f4a62d9264d5829
Vallarasu Sambathkumar <vals@live.in>
2026-01-21 11:50:52 +0530

    Update read.py

--- files/read.py
+++ files/read.py
@@ -7,25 +7,25 @@
 file = open(r"C:\Users\Valla\Desktop\temp\quotes.txt")
 
 # read entire file
-# all_quotes = file.read()
-# print(all_quotes)
+all_quotes = file.read()
+print(all_quotes)
 
-# # read line by line, better memory
-# # reads first line
-# quote = file.readline()
-# print(quote)
+# read line by line, better memory
+# reads first line
+quote = file.readline()
+print(quote)
 
-# # position set to next line
-# # reads second line
-# quote = file.readline()
-# print(quote)
+# position set to next line
+# reads second line
+quote = file.readline()
+print(quote)
 
 # reads and returns list of lines
-# list_of_quotes = file.readlines()
+list_of_quotes = file.readlines()
 
-# for q in list_of_quotes:
-#     print(q, end="")
-#     print("---")
+for q in list_of_quotes:
+    print(q, end="")
+    print("---")
 
 quote = file.readline()
 
