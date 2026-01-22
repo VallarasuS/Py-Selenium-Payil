d778eb6a7363a7ee56260dcf61b3bafddaeba90a
Vallarasu Sambathkumar <vals@live.in>
2026-01-22 12:51:41 +0530

    membership & slicing

---
+++ membership.py
@@ -0,0 +1,16 @@
+message = "Learning python is fun"
+has_fun = "fun" in message
+print("Has fun? ", has_fun)
+
+numbers = [1, 2, 3, 4, 5]
+has_five = 5 in numbers
+print("Has 5? ", has_five)
+
+fruits = ["apple", "papaya", "orange", "watermelon", "grapes"]
+contains_apple = "apple" in fruits
+print("contains apple ?", contains_apple)
+
+allergic_fruit = "grapes"
+can_drink = allergic_fruit not in fruits
+
+print("can drink", can_drink)
---
+++ slicing.py
@@ -0,0 +1,22 @@
+list_numbers = [1, 2, 3, 4, 5]
+print(list_numbers)
+
+
+print(list_numbers[4])  # zero based index access
+print(list_numbers[0:3:1])  # slicing - start:stop:step a piece of / slice of list
+
+# for i in range(0, 2, 1):
+#     print(i)
+
+tuple_numbers = (2, 4, 6, 8, 10, 12, 14, 16, 18)
+print(tuple_numbers)
+
+
+print(tuple_numbers[0])  # zero based index access
+print(tuple_numbers[0:6:1])  # slicing - start:stop:step a piece of / slice of list
+
+
+message = "Learning python is fun"
+print(message)
+print(message[0])
+print(message[0:10:1])
