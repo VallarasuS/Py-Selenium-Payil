0f1bfca344c763ed9958da454b61f3e510836099
Vallarasu Sambathkumar <vals@live.in>
2026-01-23 12:35:40 +0530

    Create file-read.py

---
+++ files/file-read.py
@@ -0,0 +1,40 @@
+# file handling
+
+# MODES
+# r -> read
+# w -> write
+# a -> append
+
+# open function -> path, mode
+
+# Read - Read All Lines as a string
+# ---------------------------------
+
+file = open(r"C:\Users\Valla\Desktop\temp\quotes.txt", "r")
+contents = file.read()
+print(contents)
+file.close()
+
+# Read Line - Read one line at a time
+# ---------------------------------
+
+quotes = open(r"C:\Users\Valla\Desktop\temp\quotes.txt", "r")
+
+line = quotes.readline()
+print(line)
+
+line = quotes.readline()
+print(line)
+
+quotes.close()
+
+# Read Lines - Read all lines as a list
+# ---------------------------------
+
+fq = open(r"C:\Users\Valla\Desktop\temp\quotes.txt", "r")
+list_quotes = fq.readlines()
+print(list_quotes)
+fq.close()
+
+print(type(list_quotes))
+print(len(list_quotes))
