4e6cdba55dc8877fd540bbd20003666616a61e58
Vallarasu Sambathkumar <vals@live.in>
2026-01-24 08:11:06 +0530

    Create while-loop.py

---
+++ while-loop.py
@@ -0,0 +1,60 @@
+# x = 1
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+# print(6)
+# print(7)
+# print(8)
+# print(9)
+# print(10)
+
+# x = 1
+# print(x)
+# x = x + 1
+# print(x)
+# x = x + 1
+# print(x)
+# x = x + 1
+# print(x)
+# x = x + 1
+# print(x)
+# x = x + 1
+# print(x)
+# x = x + 1
+# print(x)
+
+# def incr_print(num):
+#     print(num)
+#     return num + 1
+
+
+# x = 1
+# x = incr_print(x)
+# x = incr_print(x)
+# x = incr_print(x)
+# x = incr_print(x)
+
+# x = 1
+# still_downloading = True
+
+# while x <= 10:
+#     print(x)
+#     # x = x + 1
+
+# print("End")
+
+# start
+# stop
+# step
+
+start = 0
+stop = 100
+step = 5
+
+x = start
+
+while x <= stop:
+    print(x)
+    x = x + step
