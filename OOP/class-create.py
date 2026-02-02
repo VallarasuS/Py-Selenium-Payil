0128571472e82a35bc41b59856abd1724569c482
Vallarasu Sambathkumar <vals@live.in>
2026-02-02 13:05:04 +0530

    Create class-create.py

---
+++ OOP/class-create.py
@@ -0,0 +1,13 @@
+class Student:
+
+    def say_name(self):
+        return "John"
+
+
+student_one = Student()
+student_two = Student()
+student_three = Student()
+
+print(student_one.say_name())
+print(student_two.say_name())
+print(student_three.say_name())
