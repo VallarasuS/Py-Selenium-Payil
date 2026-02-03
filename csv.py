8f7149e52330732b75a281575982474a0f5ac5c1
Vallarasu Sambathkumar <vals@live.in>
2026-02-03 11:00:32 +0530

    Create csv.py

---
+++ csv.py
@@ -0,0 +1,23 @@
+# Input
+# Name, City, Math, Science, Language
+# John, Chennai, 40, 60, 50
+# Dave, Chennai, 34, 60, 50
+# Steve, Bangalore, 34, 60, 50
+
+# Expected Output
+# Name, City, Math, Science, Language, Total, Average, Top Score
+# John, Chennai, 40, 60, 50, 150, 50, 60
+# Dave, Chennai, 34, 60, 50
+# Steve, Bangalore, 34, 60, 50
+
+data = "Name, City, Math, Science, Language \n John, Chennai, 40, 60, 50 \n Dave, Chennai, 34, 60, 50 \n Steve, Bangalore, 34, 60, 50"
+
+# string
+# - split \n
+#   ignore first line
+# - split by , -> 5 tokens or words
+# process numerics, POS 2,3,4 []
+# calculate sum, average, top
+# add it back to the line
+# using join / concatenation
+# combine all lines
