6bba022c9c47924e79c11055a7ab331cf16cf062
Vallarasu Sambathkumar <vals@live.in>
2026-02-03 12:35:40 +0530

    OOP class

--- OOP/student.py
+++ OOP/student.py
@@ -1,6 +1,18 @@
 class Student:
-    pass
+
+    # initializes Student object
+    # called by python
+    # self, name -> parameters -> python provides self
+    # name -> provided by developer
+    def __init__(self, name):
+        self.name = name
 
 
-student_object = Student()
-student_object2 = Student()
+student_one = Student("John")
+# student_one.name = "John"
+
+student_two = Student("Dave")
+# student_two.name = "Dave"
+
+print(student_one.name)
+print(student_two.name)
